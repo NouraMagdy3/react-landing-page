550ba4e3717d4a9f5f748e1ffefa3185247763b3
Noura Magdy <81713174+NouraMagdy3@users.noreply.github.com>
2022-08-27 02:01:22 +0300

    CssEdits

--- src/components/subscribe/subscribe.jsx
+++ src/components/subscribe/subscribe.jsx
@@ -5,7 +5,7 @@ import './subscribe.css';
 
 const Subscribe = () => {
     return (
-        <div className='subscribe bg-white p-5 box-shadow'>
+        <div className='subscribe p-5 box-shadow'>
             <div className='d-flex justify-content-between align-items-center flex-wrap'>
                 <div>
                     <h2 className='mb-1 fw-bolder'>Subscribe Now for 
